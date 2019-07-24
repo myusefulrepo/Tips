8cf457263d13474133b24dfd8cd677a32685aa03
olivier <rapidhands@gmail.com>
2019-07-24 15:33:53 +0200

    new files
    new files

---
+++ Tips -Utiliser le Module EZLog efficacement.ps1
@@ -0,0 +1,80 @@
+﻿Import-Module EZLog
+
+############################
+# Exemple d'utilisation
+############################
+
+
+
+$Date = Get-Date -Format "dd-MM-yyyy-hh-MM-ss" 
+#$LogFile = Join-Path -Path $PSScriptRoot MylogFile-$Date.log est préférable
+$LogFile = Join-Path -Path c:\temp -ChildPath  MylogFile-$Date.log
+
+# Commencer par définir les paramètres une fois pour toutes pour se faciliter la tâche dans le script plus tard
+$PSDefaultParameterValues  = @{ 'Write-EZLog:LogFile'   = $LogFile ;  # On dit à la cmdlet Write-EZlog que son paramètre Logfile a la valeur $Logfile
+                                'Write-EZLog:Delimiter' = ';' ;       # On dit à la cmdlet Write-EZlog que son paramètre défimiter est un ;
+                                'Write-EZLog:ToScreen'  = $true }     # On dit à la cmdlet Write-EZlog que son paramètre ToScreen est à $true (affichage en console en plus d'inscription en fichier de log)
+####" une fonction est définie dans mon script
+Function Myfunction
+{
+[CmdletBinding()]
+ Param
+    (
+        # Name help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]$Name
+    )
+Begin
+    {
+    Write-EZLog -Category INF -Message "Le nom qui a été saisi est $Name"
+    }
+    Process
+    {
+    Try { # On essaie de faire ce qu'on doit faire
+        Write-Ezlog -Category INF -Message "On fait les actions qu'on a à faire sur $Name"
+        Remove-Item -Path $Name -ErrorAction stop # je fais une action totalement bidon juste pour générer une erreur
+        }
+    Catch 
+        { # si ça claque des erreurs, on les logs également 
+        $ErrorMessage = $_.Exception.Message
+        Write-EZLog -Category ERR -Message "Le message d'erreur est $ErrorMessage"
+        }
+    }
+    End
+    {
+    Write-EZLog -Category INF -Message "Les actions sont terminées"
+    }
+}
+
+#### MAIN SCRIPT
+
+# On initie la création du fichier de log
+Write-EZLog -Header
+# On appelle la fonction
+Myfunction -Name toto
+Write-EZLog -Footer # on ajoute le footer et ça ferme le fichier de log en ajoutant des infos qui vont bien
+
+<# La tête du fichier de log
++----------------------------------------------------------------------------------------+
+Script fullname          : 
+When generated           : 2019-06-12 11:50:36
+Current user             : INTRA\OF773298
+Current computer         : IS220050
+Operating System         : Microsoft Windows 7 Entreprise 
+OS Architecture          : 64 bits
++----------------------------------------------------------------------------------------+
+
+2019-06-12 11:50:36; INF; Le nom qui a été saisi est toto
+2019-06-12 11:50:36; INF; On fait les actions qu'on a à faire sur toto
+2019-06-12 11:50:36; ERR; Le message d'erreur est Cannot find path 'C:\Temp\toto' because it does not exist.
+2019-06-12 11:50:36; INF; Les actions sont terminées
+
++----------------------------------------------------------------------------------------+
+End time                 : 2019-06-12 11:50:36
+Total duration (seconds) : 0
+Total duration (minutes) : 0
++----------------------------------------------------------------------------------------+
+#>
+# C'est propre, rapide et efficace !
---
+++ Tips-CmdLets Utiles pour le réseau.ps1
@@ -0,0 +1,77 @@
+﻿# Tester la connectivité (ping)
+Test-NetConnection -ComputerName I-I2I-SQL-A0
+Test-NetConnection -ComputerName 10.8.149.135
+
+# boucle de ping
+1..254 | ForEach-Object { Test-NetConnection 10.8.249.$_ } | Format-Table -AutoSize
+
+# Tester la connectivité basé sur le port ou le service
+Test-NetConnection -ComputerName I-I2I-SQL-A0 -CommonTCPPort RDP  # (RDP, HTTP, SMB, WINRM) 
+Test-NetConnection -ComputerName I-I2I-SQL-A0  -Port 80
+# Trace route
+Test-NetConnection -ComputerName I-I2I-SQL-A0 -TraceRoute
+Test-NetConnection -ComputerName I-I2I-SQL-A0 -Hops 10
+Test-NetConnection -ComputerName I-I2I-SQL-A0 -DiagnoseRouting 
+
+# voir les routes
+Get-NetRoute 
+Get-NetRoute -InterfaceAlias Ethernet
+
+
+
+# Obtenir la configuration IP
+Get-NetIPConfiguration  
+Get-NetIPConfiguration -Detailed # équivalent à IPconfig /all
+
+# Résolution DNS 
+Resolve-DnsName -Name I-I2I-SQL-A0 
+Resolve-DnsName -Name I-I2I-SQL-A0 -Type A | Select-Object -Property Name, Type, IpAddress
+Resolve-DnsName -Name  10.8.149.135   -Type PTR
+
+# voir les connexions TCP courantes (équivalent de arp -a)
+Get-NetTCPConnection
+Get-NetTCPConnection -State Established 
+
+
+# voir les informations DNS (info de l'onglet DNS dans la conf IP)
+Get-DnsClient 
+Get-DnsClientServerAddress -AddressFamily IPv4 -InterfaceAlias Ethernet 
+
+# flush DNS cache (équivalent à ipconfig /flushDNS)
+Clear-DnsClientCache
+
+# Lister toutes les cartes réseaux
+Get-NetAdapter
+Get-NetAdapter | Select-Object -Property *
+Get-NetAdapter | Select-Object -Property Name, MacAddress, InterfaceAlias, TransmitLinkSpeed 
+Get-NetAdapter | Select-Object -Property Name, DriverVersion, DriverInformation, DriverFileName
+
+# Informations Hardware
+Get-NetAdapterHardwareInfo
+
+# Activer/désactiver une carte réseau
+Enable-NetAdapter -Name xxx
+Disable-NetAdapter -Name xxx
+Rename-NetAdapter -Name "Ethernet" -NewName "EthernetFilaire"
+
+
+Get-NetIPAddress -InterfaceIndex 4
+(Get-NetAdapter -Name "Local Area Connection" | Get-NetIPAddress).IPAddress
+Get-NetAdapter -Name "Local Area Connection" | Get-DnsClientServerAddress
+
+# Ajouter une adresse IP
+New-NetIPAddress -InterfaceAlias Ethernet -IPAddress 10.0.0.10 -DefaultGateway 10.0.0.254 -AddressFamily IPv4 -PrefixLength 24
+# changer une adresse IP existante 
+Set-NetIPAddress -InterfaceAlias Ethernet -IPAddress 10.0.0.10 -PrefixOrigin Manual
+Set-NetIPInterface -InterfaceAlias Wireless -Dhcp Enabled
+
+
+# Créer un nouveau TEAM (lbfo Team ou Load-balancing with failover team)
+New-NetLbfoTeam -Name NICTeam -TeamMembers Ethernet1, Ethernet2 -TeamingMode Lacp -TeamNicName NICTEAM -LoadBalancingAlgorithm Dynamic
+
+
+
+
+
+
+
