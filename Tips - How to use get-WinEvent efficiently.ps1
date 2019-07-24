b663b520da4bcab9924f1a22a0829ef6ac543a7e
olivier <rapidhands@gmail.com>
2019-07-24 15:30:47 +0200

    add 2 new tips
    Add 2 new tips

---
+++ Tips - How to use get-WinEvent efficiently.ps1
@@ -0,0 +1,61 @@
+﻿
+Measure-Command -Expression {
+Get-WinEvent -LogName system | where{$_.ID -eq "6005"} | Select-Object -Last 10
+}
+# about 10 second to run
+Measure-Command -Expression {
+Get-WinEvent -MaxEvents 10 -FilterHashtable @{
+                                                Logname = 'System';
+                                                id= 6005
+                                                }    
+}
+# and now 44 ms to run
+
+# Using filterhashtable is really powerful
+
+# another use with multiple values for a parameter (i.e. ID)
+Get-WinEvent -MaxEvents 10 -FilterHashTable @{
+                                LogName='System';
+                                ID='1','42'
+                                }
+
+# Get event with all error events (level LogAlways 0, Critical 1, Error 2, Warning 3, Informational 4, Verbose 5)
+Get-WinEvent -FilterHashTable @{LogName='System';Level='2'} -MaxEvents 10  | Sort-Object TimeCreated, providerName
+
+# Filter on multiple levels
+<#
+Unfortunately, you can only specify one level at a time through a hash table. 
+However, we can circumvent this problem by using the Where-Object cmdlet instead of the Level key from the hash table.
+#>
+Get-WinEvent -FilterHashtable @{LogName='system'} -MaxEvents 10 | Where-Object -FilterScript {($_.Level -eq 2) -or ($_.Level -eq 3)} 
+
+# Audit success or audit failure security events 
+<#
+Filtering events from the Security log is a bit different from other logs because it does not provide the information level. 
+Instead you can search for audit failure or audit success events. 
+You must provide this filter with the Keywords key in the hash table, and the value must be a number. 
+Here are the two audit keywords associated with their respective numbers:
+    Failure Audit 4503599627370496
+    Success audit 9007199254740992
+#>
+Get-WinEvent -FilterHashtable @{LogName='Security';Keywords='4503599627370496'} # failure audit
+
+# Events with messages containing specific words
+<#
+To display only events with messages containing a specific word, you could use the Data key. 
+However, this is a little tricky, especially because you can't work with wildcards or regular expressions. 
+The easiest way to find events with a specific word is to use the Where-Object cmdlet and filter events with the Message property.
+#>
+Get-WinEvent -FilterHashtable @{LogName='System'} | Where-Object -Property Message -Match 'the system has resumed'
+
+# Get Events with a specific date or time 
+<#
+You can display events with a specific date or time with the help of the StartTime key and/or the EndTime key inside the hash table. 
+Although there are several possibilities to provide the StartTime and EndTime values, I will only show the simplest way. 
+The first step is to store the timestamp returned by the Get-Date cmdlet into a variable.
+#>
+$StartTime = Get-Date -Year 2018 -Month 1 -Day 1  -Hour 15 -Minute 30
+$EndTime   = Get-Date -Year 2019 -Month 7 -Day 25 -Hour 12 -Minute 00
+Get-WinEvent -FilterHashtable @{LogName='System';StartTime=$StartTime;EndTime=$EndTime}
+$StartDate = Get-Date -Year 2019 -Month 7 -Day 25 -Hour 12 -Minute 00
+Get-WinEvent -FilterHashtable @{LogName='System';StartTime=$StartDate;Level='2';ID='10010'} -MaxEvents 5
---
+++ Tips - Powershell Remoting.ps1
@@ -0,0 +1,70 @@
+﻿# Tips and Tricks : POwershell remoting cheatsheet
+
+# 1 - Enabling PowerShell Remoting
+Enable-PSRemoting –force # in a console in a runasadministrator mode
+
+# 2 - Troubleshoot
+# Make sure the WinRM service is setup to start automatically
+Get-service WinRM | Select-Object -Property Name, Status, startType, DisplayName # if StarType is not Automatic then...
+Set-Service WinRM -StartMode Automatic # Set start mode to automatic
+# Another method to get the service properties
+Get-WmiObject -Class win32_service | Where-Object {$_.name -like "WinRM"}
+
+# Check if remote hosts (not in the same AD Domain) are trusted.
+# Verify trusted hosts configuration
+Get-Item WSMan:\localhost\Client\TrustedHosts # show the trusted hosts
+Set-Item WSMan:localhost\client\trustedhosts -value server01 # set up the trusted host
+Set-Item WSMan:localhost\client\trustedhosts -value " " # remove all trusted hosts
+Set-Item WSMan:\localhost\Client\TrustedHosts -Concatenate -Value Server02 # Add another trusted host in the existing list
+
+
+# 3 - Executing Remote Commands with PowerShell Remoting
+# Executing a Single Command on a Remote System
+Invoke-Command –ComputerName MyServer1 -ScriptBlock {Hostname}
+Invoke-Command –ComputerName MyServer1 -Credential demo\serveradmin -ScriptBlock {Hostname}
+Get-ADComputer -Filter *  -properties name | select @{Name="computername";Expression={$_."name"}} | Invoke-Command -ScriptBlock {hostname}
+# run scripts stored locally on your system against remote systems. 
+Invoke-Command -ComputerName MyServer1 -FilePath c:\scripts\Get-Info.ps1
+
+
+# 4 - Establishing an Interactive PowerShell Console on a Remote System
+Enter-PsSession –ComputerName server1.domain.com
+Enter-PsSession –ComputerName server1.domain.com –Credentials domain\serveradmin
+Exit-PsSession # to exit the PSSession. This will send the session into the background.
+
+# 5 - Creating Background Sessions
+New-PSSession -ComputerName server1.domain.com -Name Server1 # creating PSSession
+New-PSSession -ComputerName server1.domain.com -Name server1 -Credential domain\serveradmin
+Enter-PSSession -Name server1 # and enter
+
+# 6 - Listing Background Sessions
+Get-PSSession
+
+# 7 - Interacting with Background Sessions
+Enter-PsSession –id 3
+Enter-PSSession -Name server1
+
+# 8 - Executing Commands through Background Sessions
+Invoke-Command -Session (Get-PSSession) -ScriptBlock {Hostname} # If your goal is to execute a command on all active sessions the “Invoke-Command” and “Get-PsSession” commands can be used together.
+
+# 9 - Removing Background Sessions
+Get-PSSession | Disconnect-PSSession
+
+# 10 - Last tips
+<# 
+    Use “Invoke-Command” if you’re only going to run one command against a system
+    Use “Enter-PSSession” if you want to interact with a single system
+    Use PSsessions when you’re going to run multiple commands on multiple systems
+#>
+
+
+
+
+
+
+
+
+
+
+
+
