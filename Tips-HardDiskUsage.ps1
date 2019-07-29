08cd712d5618a3261d21464799a3154c5ab4ed5a
Rapidhands <48151863+Rapidhands@users.noreply.github.com>
2019-07-29 12:04:21 +0200

    Update Tips-HardDiskUsage.ps1

--- Tips-HardDiskUsage.ps1
+++ Tips-HardDiskUsage.ps1
@@ -1,18 +1,19 @@
 # Initialise machine name
-$server = "<>"
+$server = $env:COMPUTERNAME
  
 # Get fixed drive info
-$disks = Get-WmiObject -ComputerName $server -Class Win32_LogicalDisk -Filter "DriveType = 3";
+$disks = Get-WmiObject -ComputerName $server -Class Win32_LogicalDisk -Filter "DriveType = 3"
  
 foreach($disk in $disks)
 {
-  $deviceID = $disk.DeviceID;
-  [float]$size = $disk.Size;
-	[float]$freespace = $disk.FreeSpace;
+  $deviceID = $disk.DeviceID
+  [float]$size = $disk.Size
+	[float]$freespace = $disk.FreeSpace
  
-	$percentFree = [Math]::Round(($freespace / $size) * 100, 2);
-	$sizeGB = [Math]::Round($size / 1073741824, 2);
-	$freeSpaceGB = [Math]::Round($freespace / 1073741824, 2);
+	$percentFree = [Math]::Round(($freespace / $size) * 100, 2)
+	$sizeGB = [Math]::Round($size / 1073741824, 2)
+	$freeSpaceGB = [Math]::Round($freespace / 1073741824, 2)
  
-	Write-Host "$server,$deviceID,$sizeGB,$freeSpaceGB,$percentFree";
+	Write-Host "server,DeviceID,SizeGB,FreeSpaceGB,PercentFree"
+    Write-Host "$server,$deviceID,$sizeGB,$freeSpaceGB,$percentFree"
 }
