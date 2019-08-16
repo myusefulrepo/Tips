92a6cdc4f8ead6683ed11b0897cefbd4d2aaf72a
olivier <rapidhands@gmail.com>
2019-08-16 15:35:45 +0200

    Update Tips - How to use get-WinEvent efficiently.ps1
    update wit sample in html

--- Tips - How to use get-WinEvent efficiently.ps1
+++ Tips - How to use get-WinEvent efficiently.ps1
@@ -59,3 +59,78 @@ $EndTime   = Get-Date -Year 2019 -Month 7 -Day 25 -Hour 12 -Minute 00
 Get-WinEvent -FilterHashtable @{LogName='System';StartTime=$StartTime;EndTime=$EndTime}
 $StartDate = Get-Date -Year 2019 -Month 7 -Day 25 -Hour 12 -Minute 00
 Get-WinEvent -FilterHashtable @{LogName='System';StartTime=$StartDate;Level='2';ID='10010'} -MaxEvents 5
+
+# AN EXAMPLE WITH HTML EXPORT
+#region Define CSS style
+$css= @"
+<Style>
+h1, h5, th { text-align: center; }
+table { margin: auto; font-family: Segoe UI; box-shadow: 10px 10px 5px #888; border: thin ridge grey; }
+th { background: #0046c3; color: #fff; max-width: 400px; padding: 5px 10px; }
+td { font-size: 11px; padding: 5px 20px; color: #000; }
+tr { background: #b8d1f3; }
+tr:nth-child(even) { background: #dae5f4; }
+tr:nth-child(odd) { background: #b8d1f3; }
+</Style>
+"@
+#endregion
+
+#region Define the Export File Name
+$ReportFile = "C:\temp\LogAppView.html"
+#endregion
+
+#region Search criterias
+<# Explanations about criterias
+Level values. [System.String] separate by comma if several
+    1 : Critical
+    2 : Error
+    3 : Warning
+    4 : Information
+
+LogName = "System", "Application", "Security", ... and many other logs. [System.String] separate by comma if several
+    Get-WinEvent -ListLog *   ===> to get all log files Name
+    Get-WinEvent -ListProvider *KeyWord* to get log files containing the keyword. Look for the Name property
+
+ID  = ID of event. [System.String] separate by comma if several
+ 
+Keywords names and enumerated values 
+    Name 	            Values
+    AuditFailure 	    4503599627370496
+    AuditSuccess 	    9007199254740992
+    CorrelationHint2 	18014398509481984
+    EventLogClassic 	36028797018963968
+    Sqm 	            2251799813685248
+    WdiDiagnostic 	    1125899906842624
+    WdiContext 	        562949953421312
+    ResponseTime 	    281474976710656
+    None 	            0
+
+TimeCreated = [System.Time]
+StartDate = [System.Date]
+
+Get-WinEvent -ListLog <LogName> | Format-List -Property *  ==> to enumerate all properties
+#>
+
+# Define the filterHashTable parameters
+$StartDate = (get-date).adddays(-1)
+$LogName = "System"
+$Level = "2"
+$Id = ""
+$Keywords = ""
+# ...
+
+#endregion
+
+#region query : ADJUST AS YOU WANT
+$Query = Get-WinEvent -ErrorAction SilentlyContinue -FilterHashtable @{logname=$LogName; 
+                                                                      StartTime=$StartDate;                                                                      Level = $Level}
+#end region
+
+#region Export the result in a beautiful html page 
+$Query | 
+    ConvertTo-HTML -Head $css MachineName, ID, LevelDisplayName, level, TimeCreated, Message | 
+    Out-File -FilePath $ReportFile
+
+# and show it
+& $ReportFile
+#endregion
