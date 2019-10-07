732a6594f5f4a62ebf9ec2fa00631a9226bf3dd9
olivier <rapidhands@gmail.com>
2019-10-07 17:31:44 +0200

    Create Tips - Identify Apps Packages and remove them.ps1
    new

---
+++ Tips - Identify Apps Packages and remove them.ps1
@@ -0,0 +1,16 @@
+﻿# Get a list of the package names of all installed apps for current user
+Get-AppxPackage | Where-Object { if (-not($_.IsFramework -or $_.PublisherId -eq "cw5n1h2txyewy")) 
+                                    {$_}
+                               } | 
+                  Select-Object Name, version, PackageFullName
+
+# and now you can remove them by the following
+Remove-AppXPackage <PackageFullName>
+
+# One-liner version
+Get-AppxPackage | Where-Object { if (-not($_.IsFramework -or $_.PublisherId -eq "cw5n1h2txyewy")) 
+                                    {$_}
+                               } | 
+                  Select-Object Name, version, PackageFullName | 
+                  Out-GridView -PassThru |
+                  Remove-AppXPackage
