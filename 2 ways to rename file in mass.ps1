5bbde0f9f9855bd0e463fb1c2ce9aae33aecf558
olivier <rapidhands@gmail.com>
2019-11-14 14:10:24 +0100

    new tips
    new

---
+++ 2 ways to rename file in mass.ps1
@@ -0,0 +1,22 @@
+﻿# 2 ways to rename file in mass
+
+<# I need to rename a set of 540 files as such:
+Folder: C:\ABCD
+Original names are varied, mostly "ABCD (#).raw", but others can be "ABCD _longtext (#)"
+Destination names: ABCD-YYYY-MM-DD-HH-SS.RAW where the timestamp is the ModificationDate (not creation).
+#>
+
+
+(Get-ChildItem -File C:\abcd\abcd*.raw) |
+  Rename-Item -NewName {
+   "ABCD-{0:yyyy-MM-dd-HH-mm-ss}.raw" -f $_.LastWriteTime
+  }
+
+# With regex
+$files = Get-ChildItem -Path 'c:\temp\test' '*.raw'
+foreach ($file in $files){
+  $my_date    = "{0:yyyy-MM-dd-HH-mm-ss}" -f $file.LastWriteTime
+  $Begin_name = [regex]::Match($file.name,'^([A-Z]+)[\d\(_\s].+\.raw$').groups[1].value
+  $New_name   = $Begin_name +'-'+ $my_date + '.RAW'
+  Rename-Item -Path $file.fullname -NewName $New_name -WhatIf
+}
---
+++ Tips- Useful Events to Monitor for Security Reasons.ps1
@@ -0,0 +1,89 @@
+﻿# Useful Events to monitor
+
+# 1 - System Events
+<# 
+ID 104 An event log was cleared
+#>
+$SystemEvents = Get-WinEvent -FilterHashtable @{
+                               LogName = "System"
+                               id = "104"
+                               } -MaxEvents 50 -ErrorAction SilentlyContinue
+
+# 2 - Security Events
+<# 
+ID 4656 - Auditing of configured files, registry keys:
+                PowerShell profiles (*profile*.ps1)
+                Security settings (HKLM:\Software\Policies\*)
+#>
+$SecurityEvents = Get-WinEvent -FilterHashtable @{
+                               LogName = "Security"
+                               id = "4656"
+                               } -MaxEvents 50 -ErrorAction SilentlyContinue
+
+# 3 - Windows Powershell Events
+<#
+ID 400 - PowerShell Startup, including hosting application, version
+ID 800 - Command and Parameter Logging
+#>
+$PSEvents = Get-WinEvent -FilterHashtable @{
+                               LogName = "Windows Powershell"
+                               id = "400", "800"
+                               } -MaxEvents 50 -ErrorAction SilentlyContinue
+
+# 3 - Microsoft-Windows-PowerShell/Operational Events
+<#
+ID 4104 - Warning - ScriptBlock automatic logging – used APIs or techniques commonly associated with malware
+ID 4104 - Verbose - ScriptBlock logging
+ID 53507 - PowerShell debugger attached to a process
+
+Nota : 4104/Warning is NOT a replacement for an intrusion detection system.
+#>
+$PSOperationalEvents = Get-WinEvent -FilterHashtable @{
+                               LogName = "Microsoft-Windows-Powershell/Operational"
+                               id = "4104","53507"
+                               } -ErrorAction SilentlyContinue
+
+# 4 - Microsoft-Windows-WinRM/Operational Events
+<# 
+ID 91 - User connected to system with PowerShell Remoting
+#>
+
+$WinRMEvents = Get-WinEvent -FilterHashtable @{
+                               LogName = "Microsoft-Windows-WinRM/Operational"
+                               id = "91"
+                               } -ErrorAction SilentlyContinue
+
+$Result =@()
+$Result += $SecurityEvents
+$Result += $PSEvents 
+$Result += $PSOperationalEvents
+$Result += $WinRMEvents
+$Result
+
+# Source Jon Fow  in "Security 102 - Defending Against Powershell Attacks" (SEC102-PowerShell-Attacks.pptx)
+# Adapted from a presentation by Lee Holmes, Lead Security Architect, Azure Management
+# Ref. : https://github.com/rtpsug/PowerShell-Saturday/blob/master/2019-NC.State/2019-09-21-PowerShell.Security.102-Jon.Fox/SEC102-PowerShell-Attacks.pptx
+
+
+# ADDENDUM
+<#
+The PowerShell Injection Hunter module looks for many instances of unsafe coding practices when
+PowerShell scripts are exposed to untrusted input
+
+https://devblogs.microsoft.com/powershell/powershell-injection-hunter-security-auditing-for-powershell-scripts/
+
+- Install Module : Install-Module InjectionHunter
+- Retreive Install Path : Get-Module InjectionHunter -List | Foreach-Object Path
+- Create a file Named PSScriptAnalyzerSettings.psd1 in a location of your choice and put the following content inside it : 
+@{
+ IncludeDefaultRules = $true
+ CustomRulePath = "Your\Retreive\Install\Path\InjectionHunter.psd1"
+}
+- In VS code, Modify the User Settings with the following line
+"powershell.scriptAnalysis.settingsPath": "Your\Path\ToFile\PSScriptAnalyzerSettings.psd1"
+
+When you open a PowerShell script with possible code injection risks, 
+you should now see Script Analyzer warnings that highlight what they are and how to fix them.
+
+Ref. : https://devblogs.microsoft.com/powershell/powershell-injection-hunter-security-auditing-for-powershell-scripts/
+#>
