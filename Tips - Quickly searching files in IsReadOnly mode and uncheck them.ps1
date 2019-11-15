4c2db1ef346a70ed1c33424867f0645175675758
olivier <rapidhands@gmail.com>
2019-11-15 17:41:03 +0100

    Create Tips - Quickly searching files in IsReadOnly mode and uncheck them.ps1
    new

---
+++ Tips - Quickly searching files in IsReadOnly mode and uncheck them.ps1
@@ -0,0 +1,12 @@
+﻿# Searching for All Files in ReadOnly Mode and uncheck IsReadOnly Property
+Get-ChildItem c:\temp -Recurse | Set-ItemProperty -Name IsReadOnly -Value $false -ErrorAction SilentlyContinue
+
+# Searching for All specific extension Files in ReadOnly Mode and uncheck IsReadOnly Property
+Get-ChildItem c:\temp -Filter *.docx | Set-ItemProperty -Name IsReadOnly -Value $false
+
+
+# We could use the same way to perform other changes on the files properties
+
+
+
+
