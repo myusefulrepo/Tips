77e4d956aa2f967dee43909b7e373ad8818ab0ae
Rapidhands <48151863+Rapidhands@users.noreply.github.com>
2023-06-27 08:10:06 +0200

    Function Convert HashTable to PSObject
    Useful function to keep in mind

---
+++ Function ConvertHashtableTo-Object.ps1
@@ -0,0 +1,22 @@
+﻿# source : https://gordon.byers.me/powershell/convert-a-powershell-hashtable-to-object/
+function ConvertHashtableTo-Object
+{
+    [CmdletBinding()]
+    Param([Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelinebyPropertyName = $True)]
+        [hashtable]$ht
+    )
+    PROCESS
+    {
+        $results = @()
+
+        $ht | ForEach-Object {
+            $result = New-Object psobject
+            foreach ($key in $_.keys)
+            {
+                $result | Add-Member -MemberType NoteProperty -Name $key -Value $_[$key]
+            }
+            $results += $result
+        }
+        return $results
+    }
+}
